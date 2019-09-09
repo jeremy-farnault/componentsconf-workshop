65f3d7c1a5294a47907d70a96e7c075c3fb96e7a
Jeremy Farnault <haskkor@gmail.com>
2019-09-09 16:22:53 +1000

    Tutorial over

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,36 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateCoin = `subscription OnCreateCoin {
-  onCreateCoin {
-    id
-    clientId
-    name
-    symbol
-    price
-  }
-}
-`;
-export const onUpdateCoin = `subscription OnUpdateCoin {
-  onUpdateCoin {
-    id
-    clientId
-    name
-    symbol
-    price
-  }
-}
-`;
-export const onDeleteCoin = `subscription OnDeleteCoin {
-  onDeleteCoin {
-    id
-    clientId
-    name
-    symbol
-    price
-  }
-}
-`;
 export const onCreateNote = `subscription OnCreateNote($owner: String!) {
   onCreateNote(owner: $owner) {
     id
